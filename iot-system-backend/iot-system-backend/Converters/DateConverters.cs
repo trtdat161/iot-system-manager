f728072bedc6803887e8ed901c42228097f9cf09
Tran Tien Dat <trtdat161@gmail.com>
2026-04-05 22:07:20 +0700

    first commit

---
+++ iot-system-backend/iot-system-backend/Converters/DateConverters.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace IoT_system.Converters
+{
+    public class DateConverters : JsonConverter<DateOnly>
+    {
+        private string format = "dd/MM/yyyy";
+
+        // ham Read doc chuoi json bien thanh DateOnly
+        public override DateOnly Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            // bien doi
+            return DateOnly.ParseExact(reader.GetString(), format, CultureInfo.InvariantCulture);// CultureInfo.InvariantCulture không phu thuoc vao location
+        }
+
+        public override void Write(Utf8JsonWriter writer, DateOnly value, JsonSerializerOptions options)
+        {
+            writer.WriteStringValue(value.ToString(format));
+        }
+    }
+}
---
+++ iot-system-backend/iot-system-backend/Program.cs
@@ -0,0 +1,57 @@
+﻿using IoT_system.Converters;
+using Microsoft.AspNetCore.Localization;
+using System.Globalization;
+
+/* ======================================== BUILDER ==================================== */
+
+var builder = WebApplication.CreateBuilder(args);
+// add cors policy để sau này FE gọi được
+builder.Services.AddCors(option =>
+{
+    option.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins("http:...")// demo tạm, lấy url FE js
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+builder.Services.AddControllers().AddJsonOptions(option =>
+{
+    option.JsonSerializerOptions.Converters.Add(new DateConverters()); // my function convert
+});
+// khai báo web api để sau dùng các services
+builder.Services.AddControllers();
+
+// services dưới đây ...
+
+
+/* ======================================== APP ==================================== */
+
+var app = builder.Build();
+// enable cors
+app.UseCors();
+
+// khai bao ngon ngu
+var cultures = new CultureInfo[]
+{
+    // vi-VN, en-US, (tạo mảng chưa chứa 2 ngôn ngữ)
+    new CultureInfo("vi-VN"),
+    new CultureInfo("en-US"),
+};
+app.UseRequestLocalization(new RequestLocalizationOptions
+{
+    // cau hinh ngon ngu mac dinh
+    DefaultRequestCulture = new RequestCulture(cultures[0]),
+    SupportedCultures = cultures,
+    SupportedUICultures = cultures
+});
+app.UseSession();// cho phep dung session cho session len truoc midlewar thi moi co ma sai
+
+// ------- su dung midleware -------
+//app.UseMiddleware<>();// dua security len tren dau
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller}/{action}"
+);
+app.Run();
